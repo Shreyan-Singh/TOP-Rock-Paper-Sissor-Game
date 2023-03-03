a1c52b6ebc51e92ff4bf81d7f4da58e8b5b6ba2f
Shreyan-Singh <shreysingh2055@gmail.com>
2023-03-04 01:33:52 +0545

    game is working fine need to improve ui

--- script.js
+++ script.js
@@ -1,13 +1,43 @@
 
 
+
+
+window.playerScore=0;
+    window.computerScore=0;
+window.count=0;
+
+function reset(){
+window.playerScore=0;
+    window.computerScore=0;
+window.count=0;
+const Win=document.getElementById('winner');
+            Win.textContent="";
+}
+
 function getComputerChoice(){
     let computerChoice;
     let choices=["rock","paper","sissor"];
     computerChoice=choices[Math.floor(Math.random()*3)];
     return computerChoice;
 }
+const onClick = (event) => {
+    
+    
+    const playerSelection=(event.target.textContent).toLowerCase();
+    const computerSelection=getComputerChoice();
+    console.log(count);
+    if(count<5){
+    playRound(playerSelection,computerSelection);
+    
+   
+}
+  }
+  window.addEventListener('click', onClick);
+   
+
 function playRound(playerSelection,computerSelection){
 
+    
     if(playerSelection===computerSelection){
         console.log("daw");
         playerScore=playerScore+0;
@@ -46,13 +76,36 @@ function playRound(playerSelection,computerSelection){
         computerScore=computerScore+0;
     }
     
+    const yourScore=document.getElementById('yourScore');
+    yourScore.textContent=playerScore;
+
+    const computerP=document.getElementById('computerScore');
+    computerP.textContent=computerScore;
+
+    if(count===4){
+        if(playerScore>computerScore){
+            const playerWin=document.getElementById('winner');
+            playerWin.textContent="You Won";
+        }
+        if(playerScore<computerScore){
+            const computerWin=document.getElementById('winner');
+            computerWin.textContent="Computer Won";
+        }
+        if(playerScore===computerScore){
+            const Win=document.getElementById('winner');
+            Win.textContent="Draw. You and Computer have same score.";
+        }
+    }
+    count=count+1;
+
 }
+/*
 //this staets the game
 function game(){
     window.playerScore=0;
     window.computerScore=0;
     for(let i=0; i<5; i++){
-        const playerSelection=prompt().toLocaleLowerCase();
+        
         const computerSelection=getComputerChoice();
         playRound(playerSelection, computerSelection);
     }
@@ -69,4 +122,5 @@ function game(){
     
 
 }
+*/
 
